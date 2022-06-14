52802b302a7163423b9fc0f5dc1606c2c0d6ba87
Farukh Rakhmatullaev <joombey@yandex.ru>
2022-06-14 18:14:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/genshinimpactplanner/fragments/BlankViewModel.java
@@ -0,0 +1,8 @@
+package com.example.genshinimpactplanner.fragments;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+public class BlankViewModel extends ViewModel {
+    private LiveData<Integer> kokasd;
+}
